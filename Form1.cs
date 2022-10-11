07e9d05806139b93c7deb5513f7bc65468137a3e
aryanprince <aryanprincesanty@icloud.com>
2022-10-11 10:47:08 +0100

    Testing out lines and shapes

--- Form1.cs
+++ Form1.cs
@@ -9,9 +9,19 @@ namespace ASE_Assignment
 
         private void pictureBox1_Paint(object sender, PaintEventArgs e)
         {
+            Point pt1 = new Point(0, 0);
+            Point pt2 = new Point(200, 200);
+            Rectangle rect1 = new Rectangle(50, 80, 100, 130);
             Graphics g = e.Graphics;
-            Pen p = new Pen(Color.Black, 2);
-            g.DrawLine(p, 0, 0, 100, 100);
+            Pen myBlackPen = new Pen(Color.Black, 3);
+
+            g.DrawLine(myBlackPen, pt1, pt2);
+            g.DrawLine(myBlackPen, 0, 50, 200, 50);
+
+            g.DrawEllipse(myBlackPen, 50, 50, 200, 100);
+
+            g.DrawRectangle(myBlackPen, rect1);
+            myBlackPen.Dispose();
         }
     }
 }
