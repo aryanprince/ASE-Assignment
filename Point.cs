abc00cd8d864bc00c0c0ad981cf3b2f6bbd24b6b
Aryan Prince <aryanprincesanty@icloud.com>
2022-10-24 22:00:35 +0100

    fix: Shape class now inherits from Point

---
+++ Point.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASE_Assignment
+{
+    public class Point
+    {
+        protected int x, y;
+
+        public Point()
+        {
+            x = 50;
+            y = 50;
+        }
+
+        public Point(int xValue, int yValue)
+        {
+            x = xValue;
+            y = yValue;
+        }
+
+    }
+}
