2950922c8e816e73b84d87f3fef9e557ea17a8ca
Aryan Prince <aryanprincesanty@icloud.com>
2022-10-28 17:36:40 +0100

    chore: Removed Color and X,Y pos from Shape

--- Shape.cs
+++ Shape.cs
@@ -4,21 +4,6 @@ namespace ASE_Assignment
 {
     public abstract class Shape
     {
-        //protected Color s_defaultColour = Color.Green;
-        //protected int x, y;
-        //public Color Color { get; set; }
-
-        //public int X
-        //{
-        //    get { return x; }
-        //    set { x = value; }
-        //}
-        //public int Y
-        //{
-        //    get { return y; }
-        //    set { y = value; }
-        //}
-
         protected Point s_defaultPosition = new Point(0,0);
         public Point Position { get; set; }
 
