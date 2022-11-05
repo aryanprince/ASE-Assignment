b0822ec28dd507a6b9fe3919a0bb4207e5d7c59a
Aryan Prince <aryanprincesanty@icloud.com>
2022-11-05 15:40:30 +0000

    style: Updated assembly info with new names

--- Main-Project/Properties/AssemblyInfo.cs
+++ Main-Project/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ASE-Assignment")]
+[assembly: AssemblyTitle("ASE_Assignment")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ASE-Assignment")]
+[assembly: AssemblyProduct("ASE_Assignment")]
 [assembly: AssemblyCopyright("Copyright ©  2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
