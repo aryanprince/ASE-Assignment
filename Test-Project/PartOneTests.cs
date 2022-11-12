5e41968530292252af54de67c260d83a0e46eae8
Aryan Prince <aryanprincesanty@icloud.com>
2022-11-12 14:34:14 +0000

    added more rigorous line tests

--- Test-Project/PartOneTests.cs
+++ Test-Project/PartOneTests.cs
@@ -584,7 +584,7 @@ namespace Unit_Tests
         private readonly Parser _parser = new Parser();
 
         [TestMethod()]
-        public void ParseAction_DrawingLineWithParameters()
+        public void ParseAction_DrawingLine_WithParameters()
         {
             //arrange
             string input = "drawto 125 210";
@@ -602,7 +602,7 @@ namespace Unit_Tests
         }
 
         [TestMethod()]
-        public void ParseAction_DrawingLineWithNoParameters()
+        public void ParseAction_DrawingLine_WithNoParameters()
         {
             //arrange
             string input = "drawto";
@@ -620,7 +620,25 @@ namespace Unit_Tests
         }
 
         [TestMethod()]
-        public void ParseInput_DrawingLineWithParameters()
+        public void ParseAction_DrawingLine_WithDifferentCase()
+        {
+            //arrange
+            string input = "dRaWtO 125 210";
+
+            // act
+            Action action = _parser.ParseAction_Command(input);
+
+            // assert
+            Assert.IsNotNull(action);
+            Assert.AreEqual(Action.drawto, action);
+            Assert.AreNotEqual(Action.circle, action);
+            Assert.AreNotEqual(Action.rectangle, action);
+            Assert.AreNotEqual(Action.square, action);
+            Assert.AreNotEqual(Action.triangle, action);
+        }
+
+        [TestMethod()]
+        public void ParseInput_DrawingLine_WithParameters()
         {
             //arrange
             string input = "drawto 125 210";
@@ -638,6 +656,85 @@ namespace Unit_Tests
             Assert.AreEqual(125, command.ActionValues[0]);
             Assert.AreEqual(210, command.ActionValues[1]);
             Assert.AreEqual(2, command.ActionValues.Length);
+        }
+
+        [TestMethod()]
+        public void ParseInput_DrawingLine_WithDifferentCase()
+        {
+            //arrange
+            string input = "dRaWtO 125 210";
+
+            // act
+            Command command = _parser.ParseInput_SingleLine(input);
+
+            // assert
+            Assert.IsNotNull(command);
+            Assert.AreEqual(Action.drawto, command.ActionWord);
+            Assert.AreNotEqual(Action.circle, command.ActionWord);
+            Assert.AreNotEqual(Action.rectangle, command.ActionWord);
+            Assert.AreNotEqual(Action.square, command.ActionWord);
+            Assert.AreNotEqual(Action.triangle, command.ActionWord);
+            Assert.AreEqual(125, command.ActionValues[0]);
+            Assert.AreEqual(210, command.ActionValues[1]);
+            Assert.AreEqual(2, command.ActionValues.Length);
+        }
+
+        [TestMethod()]
+        public void ParseInput_CreatingLineInstance_WithParameters()
+        {
+            //arrange
+            string input = "drawto 125 210";
+
+            // act
+            Command command = _parser.ParseInput_SingleLine(input);
+            ShapeFactory shapeFactory = new ShapeFactory();
+            Cursor cursor = new Cursor();
+            Shape shape = shapeFactory.CreateShape(command, cursor.Position, cursor.Fill, cursor.PenColor);
+
+            // assert
+            Assert.IsNotNull(shape);
+            Assert.IsInstanceOfType(shape, typeof(Line));
+            Assert.IsNotInstanceOfType(shape, typeof(Square));
+            Assert.IsNotInstanceOfType(shape, typeof(Rectangle));
+            Assert.IsNotInstanceOfType(shape, typeof(Circle));
+            Assert.IsNotInstanceOfType(shape, typeof(Triangle));
+            // checks that the values are set to default values as they are not specified in the input
+            Assert.AreEqual(shape.DefaultFill, shape.Fill);
+            Assert.AreEqual(shape.DefaultPenColor, shape.PenColor);
+            Assert.AreEqual(shape.DefaultPosition, shape.Position);
+            // manually checking the values to check that they are set correctly
+            Assert.AreEqual(false, shape.Fill);
+            Assert.AreEqual(Color.Red, shape.PenColor);
+            Assert.AreEqual(new Point(0, 0), shape.Position);
+        }
+
+        [TestMethod()]
+        public void ParseInput_CreatingLineInstance_WithDifferentCase()
+        {
+            //arrange
+            string input = "dRaWtO 125 210";
+
+            // act
+            Command command = _parser.ParseInput_SingleLine(input);
+            ShapeFactory shapeFactory = new ShapeFactory();
+            Cursor cursor = new Cursor();
+            Shape shape = shapeFactory.CreateShape(command, cursor.Position, cursor.Fill, cursor.PenColor);
+
+            // assert
+            Assert.IsNotNull(shape);
+            Assert.IsInstanceOfType(shape, typeof(Line));
+            Assert.IsNotInstanceOfType(shape, typeof(Square));
+            Assert.IsNotInstanceOfType(shape, typeof(Rectangle));
+            Assert.IsNotInstanceOfType(shape, typeof(Circle));
+            Assert.IsNotInstanceOfType(shape, typeof(Triangle));
+            // checks that the values are set to default values as they are not specified in the input
+            Assert.AreEqual(shape.DefaultFill, shape.Fill);
+            Assert.AreEqual(shape.DefaultPenColor, shape.PenColor);
+            Assert.AreEqual(shape.DefaultPosition, shape.Position);
+            // manually checking the values to check that they are set correctly
+            Assert.AreEqual(false, shape.Fill);
+            Assert.AreEqual(Color.Red, shape.PenColor);
+            Assert.AreEqual(new Point(0, 0), shape.Position);
         }
     }
 }
